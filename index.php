e454ae6be9051163795db9c21d0af43fa353dc69
xanlimit <haroldbongjingchoy98@outlook.com>
2021-01-16 03:41:57 +0800

    UI changes

--- index.php
+++ index.php
@@ -5,29 +5,41 @@ include("_header.php");
 include("_navbar.php");
 ?>
 
-<div class="container" style="padding-top: 10%;">
-  <form action="login.php" method="POST">
-    <div class="row">
-      <div class="col-6">
-        <label for="email">Email: </label>
-      </div>
-      <div class="col-6">
-        <input type="email" name="email" id="email">
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-6">
-        <label for="password">Password: </label>
-      </div>
-      <div class="col-6">
-        <input type="password" name="password" id="password">
-      </div>
-    </div>
-    <input type="submit" value="Login">
-  </form>
+<div class="row">
+	<div class="col-md-4"></div>
+		<div class="col-md-4" >
+			<div style="display: flex; justify-content: center; align-items: center;padding:2rem;padding-top:20%;">
+				<img src="assets/img/Logo.ico"></img>
+			</div>
+			<div class="card card-black">
+			  <div class="card-header">
+				<h3 class="card-title">Log In</h3>
+			  </div>
+			<!-- form start -->
+				  <form class="form-horizontal" action="login.php" method="POST">
+					<div class="card-body">
+					  <div class="form-group row">
+						<div class="col-sm-12">
+						  <input type="email" class="form-control" id="email" name="email" placeholder="Email">
+						</div>
+					  </div>
+					  <div class="form-group row">
+						<div class="col-sm-12">
+						  <input type="password" class="form-control" id="password" name="password" placeholder="Password">
+						</div>
+					  </div>
+					</div>
+					<!-- /.card-body -->
+					<div class="card-footer">
+					  <button type="submit" class="btn btn-black" value="Login">Sign in</button>
+					  <button type="submit" class="btn btn-default float-right">Cancel</button>
+					</div>
+					<!-- /.card-footer -->
+				  </form>
+			</div>
+		</div>
+	<div class="col-md-4"></div>
 </div>
-
-
 <?php
 include("_footer.php");
 ?>
